e8121ccddce4233aec74a88aa700901ff942d711
Jozef Carruthers <jxc1389@student.bham.ac.uk>
2024-01-03 15:20:28 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css';
+import logo from './assets/images/flowicon.png';
+
+// Plant Data to be rendered - eventually will be dependent on what is saved for a user
+const plantData = [
+  {
+    plantName:"Cactus",
+    calendarImg:logo,
+    alt:"Flow Logo",
+    daysTilWater:40
+  },
+  {
+    plantName:"Rose",
+    calendarImg:logo,
+    alt:"Flow Logo",
+    daysTilWater:6
+  },
+  {
+    plantName:"Willow",
+    calendarImg:logo,
+    alt:"Flow Logo",
+    daysTilWater:0
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Flow</h1>
+      <p>A Newcomers Guide to Gardening</p>
+      {/* Navbar here for any other pages - for now just the main page  */}
+
+      <section className="plants">
+      {/* Each plant calendar component here, add plant button at the end */}
+      {plantData.map(plant => <PlantCalendar plantObj={plant} key={plant.plantName} />)}
+      </section>
     </div>
+  );
+}
+
+function PlantCalendar(props){
+  return (
+  <div className="plant-card">
+    <div className="plant-image">
+      <img src={props.plantObj.calendarImg}  alt={props.plantObj.alt} />
+    </div>
+    <div className="plant-info">
+      <h2>{props.plantObj.plantName}</h2>
+      <p>{props.plantObj.daysTilWater}</p>
+    </div>
+  </div>
   );
 }
 
